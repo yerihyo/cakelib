74a22c3097fa1c1d5264e91713e1d0c662678033
Moonyoung Kang <yerihyo@gmail.com>
2024-10-21 14:37:04 -0700

    updated

--- lib/function/function_tool.ts
+++ lib/function/function_tool.ts
@@ -43,21 +43,26 @@ export default class FunctionTool{
   static fn12f11 = <X, A extends any[], R>(fn1:(l:X[], ...args:A) => R):((x:X, ...args:A) => R) =>
     (x:X, ...args:A):R => fn1 == null ? undefined : fn1?.(x == null ? (x as X[]) : [x], ...args);
   
-  static f_decider2fn1_every = <X>(f_decider:(x:X) => boolean,) => (l:X[]) => l?.every(f_decider);
-  static f11_decider2fn1_every =  <X, A extends any[], R>(
-    f11:(x:X, ...args:A) => R,
-    f_decider: (r:R) => boolean,
-  ):((l:X[], ...args:A) => boolean) => lodash.flow(
-    FunctionTool.f112fnn(f11),
-    FunctionTool.f_decider2fn1_every(f_decider),
-  )
+  static f_decider2f_every = <X, A extends any[]>(f_decider:(x:X, ...args:A) => boolean,) =>
+    (l:X[], ...args:A) => l?.every(x => f_decider(x, ...args));
 
-  static f1b2fn1_every = <X, A extends any[]>(
-    f11:(x:X, ...args:A) => boolean,
-  ):((l:X[], ...args:A) => boolean) => lodash.flow(
-    FunctionTool.f112fnn(f11),
-    FunctionTool.f_decider2fn1_every(Boolean),
-  )
+  static f_decider2f_some = <X, A extends any[]>(f_decider:(x:X, ...args:A) => boolean,) =>
+    (l:X[], ...args:A) => l?.some(x => f_decider(x, ...args));
+  
+  // static f11_decider2fn1_every =  <X, A extends any[], R>(
+  //   f11:(x:X, ...args:A) => R,
+  //   f_decider: (r:R) => boolean,
+  // ):((l:X[], ...args:A) => boolean) => lodash.flow(
+  //   FunctionTool.f112fnn(f11),
+  //   FunctionTool.f_decider2fn1_every(f_decider),
+  // )
+
+  // static f1b2fn1_every = <X, A extends any[]>(
+  //   f11:(x:X, ...args:A) => boolean,
+  // ):((l:X[], ...args:A) => boolean) => lodash.flow(
+  //   FunctionTool.f112fnn(f11),
+  //   FunctionTool.f_decider2fn1_every(Boolean),
+  // )
 
   // static f_skip2wrapper = <O, A extends any[]>(
   //     f_skip:(...args:A) => boolean,
