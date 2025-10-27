1d4bfb43630583d08a1484f6d08b51f90bcab422
Moonyoung Kang <yerihyo@gmail.com>
2025-10-27 11:34:05 -0700

    (n1409) updated

--- lib/csv/csv_tool.ts
+++ lib/csv/csv_tool.ts
@@ -1,3 +1,5 @@
+import DictTool from '../collection/dict/dict_tool';
+import MathTool from '../number/math/math_tool';
 import {stringify} from 'csv-stringify/sync';
 
 export default class CsvTool{
@@ -9,4 +11,14 @@ export default class CsvTool{
     const newline = '\r\n';
     return new Blob(["\ufeff" + str_ll.map(strs => strs.join(',')).join(newline)+newline], { type: 'text/csv; charset=utf-8' })
   }
+
+  static str_ll2objs = (data: (string|number)[][]): Record<string,string|number>[] => {
+    return data == null
+      ? undefined
+      : MathTool.lt(data?.length, 1)
+        ? []
+        : data.slice(1)
+            .map(row => DictTool.listpair2dict<string,string|number>(data[0] as string[], row));
+  }
+
 }
